9a1cddced96631c4af950689b02bbdc9f15505e9
Lucas Samuel <lucassamuel.dev@gmail.com>
2023-12-11 16:23:02 -0300

    create structure component Hero

---
+++ src/components/hero.tsx
@@ -0,0 +1,16 @@
+export const Hero = () => {
+  return (
+    <div className="container">
+      <figure>
+        <img src='hero-image.png' alt='Ilustração de uma pessoa clicando no notebook' />
+      </figure>
+
+      <div>
+        <h2>Encontre os melhores artigos de programação em um só lugar</h2>
+        <p>Explore o topo da programação em um só lugar! Seu destino único para dicas e tendências atuais.</p>
+
+        <button>Buscar artigos</button>
+      </div>
+    </div>
+  )
+}
